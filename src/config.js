33d3b3ce612d7103f81b8bc233619566d0d69bef
Ankush Singh <ankush4singh@gmail.com>
2022-09-30 10:01:25 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// Pwease dont use these for something bwad >.<
 export const host = "tincture-db.ccqptblwinr0.ap-south-1.rds.amazonaws.com";
 export const user = "admin";
 export const pass = "192837465.abcd.";
